6e01ed5bbfe67fa443a9bf954baee64e52e15f7a
winsa24 <67416652+winsa24@users.noreply.github.com>
2020-11-11 07:35:49 +0100

    vice-versa

--- space/js/animate.js
+++ space/js/animate.js
@@ -1,14 +1,27 @@
-function move() {
-    const img = document.getElementById('img');
-    let windowsWidth = window.innerWidth;
-    if(img.offsetLeft < windowsWidth){
-        img.style.left = img.offsetLeft + 20 + "px";
-    }else{
-        img.style.left = 0 + "px";
-    }
-
-}
+var flag = 0;
 window.onload = function(){
     setInterval(move, 1000);
 }
+function move() {
+    const img = document.getElementById('img');
+    let speed = document.getElementById('speed').value;
+    let windowsWidth = window.innerWidth;
+
+    if(img.offsetLeft > windowsWidth){
+        flag = 1;
+        img.style.transform = 'rotate(180deg)';
+    }
+    if(img.offsetLeft < 0){
+        flag = 0;
+        img.style.transform = 'rotate(180deg)';
+    }
+
+    if(flag == 0){
+        img.style.left = img.offsetLeft + parseInt(speed) + "px";
+    }else{
+        img.style.left = img.offsetLeft - parseInt(speed) + "px";
+    }
+
+}
+
 
